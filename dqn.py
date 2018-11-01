c8ba971554def7bb9dca218dbf0c0d534a3aaf1c
Olav Markussen <olavmarkussen@hotmail.no>
2018-11-01 15:44:47 +0100

    Fix some stuff

--- dqn.py
+++ dqn.py
@@ -40,7 +40,7 @@ class DQNAgent:
         act_values = self.model.predict(state)
         return np.argmax(act_values[0])  # returns action
 
-    def replay(self, batch_size):
+    def train_from_replay_memory(self, batch_size):
         minibatch = random.sample(self.memory, batch_size)
         for state, action, reward, next_state, done in minibatch:
             target = reward
@@ -59,32 +59,53 @@ class DQNAgent:
     def save(self, name):
         self.model.save_weights(name)
 
+def render(agent):
+    env = gym.make('CartPole-v1')
+    state_size = env.observation_space.shape[0]
+    action_size = env.action_space.n
+    done = False
+    batch_size = 32
+
+    state = env.reset()
+    state = np.reshape(state, [1, state_size])
+    for time in range(500):
+        env.render()
+        action = agent.act(state)
+        next_state, _, done, _ = env.step(action)
+        next_state = np.reshape(next_state, [1, state_size])
+        state = next_state
+        if done:
+            #env.close()
+            return
+    #env.close()
+    return
 
 if __name__ == "__main__":
     env = gym.make('CartPole-v1')
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
     agent = DQNAgent(state_size, action_size)
-    # agent.load("./save/cartpole-dqn.h5")
     done = False
     batch_size = 32
 
     for e in range(EPISODES):
         state = env.reset()
         state = np.reshape(state, [1, state_size])
+
+        if (e % 10) == 0:
+            render(agent)
         for time in range(500):
-            # env.render()
             action = agent.act(state)
             next_state, reward, done, _ = env.step(action)
             reward = reward if not done else -10
             next_state = np.reshape(next_state, [1, state_size])
             agent.remember(state, action, reward, next_state, done)
             state = next_state
+
             if done:
                 print("episode: {}/{}, score: {}, e: {:.2}"
                       .format(e, EPISODES, time, agent.epsilon))
                 break
-            if len(agent.memory) > batch_size:
-                agent.replay(batch_size)
-        # if e % 10 == 0:
-        #     agent.save("./save/cartpole-dqn.h5")
+
+            if (e % 2 == 0) and len(agent.memory) > batch_size:
+                agent.train_from_replay_memory(batch_size)
