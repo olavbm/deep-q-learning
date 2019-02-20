65f3a2be20e2b2ec8c77ebffa9ab27156eb08222
Olav Markussen <olavmarkussen@hotmail.no>
2019-02-20 18:45:05 +0100

    Use the config instead of local constants

--- trainer.py
+++ trainer.py
@@ -1,19 +1,18 @@
-from pandas.compat.numpy import np
-
+import numpy as np
 from util import render
 
 
-def train(env, agent, EPISODES):
+def train(env, agent, config):
     batch_size = 32
     state_size = env.observation_space.shape[0]
-    for e in range(EPISODES):
+    for e in range(config.episodes):
         state = env.reset()
         state = np.reshape(state, [1, state_size])
 
         if (e % 20) == 0:
             render(agent)
 
-        for time in range(500):
+        for time in range(config.time_limit):
             action = agent.act(state)
             next_state, reward, done, _ = env.step(action)
             reward = reward if not done else -10
@@ -24,7 +23,7 @@ def train(env, agent, EPISODES):
             if done:
                 print(
                     "episode: {:4}/{}, score: {:4}, e: {:.2}".format(
-                        e, EPISODES, time, agent.epsilon
+                        e, config.episodes, time, agent.epsilon
                     )
                 )
                 break
