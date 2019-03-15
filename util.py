8731a09bc12094996a56d03b94b7ea5edf44dca8
Olav Markussen <olavmarkussen@hotmail.no>
2019-03-15 20:39:57 +0100

    Move away from parallel viz and training

--- util.py
+++ util.py
@@ -1,22 +1,31 @@
 import gym
+import time
 import numpy as np
 
-def render(agent):
-    env = gym.make('CartPole-v1')
-    state_size = env.observation_space.shape[0]
-    action_size = env.action_space.n
-    done = False
-    batch_size = 32
 
-    state = env.reset()
-    state = np.reshape(state, [1, state_size])
-    for time in range(500):
-        env.render()
-        action = agent.act(state)
-        next_state, _, done, _ = env.step(action)
-        next_state = np.reshape(next_state, [1, state_size])
-        state = next_state
-        if done:
-            return
-    return
+def render(current_agent):
+    while True:
+        # print(current_agent)
+        # print(current_agent.value)
+        # print(current_agent.get())
+        # agent = current_agent.get()
+        agent = current_agent
+        # print(agent, "agent")
+        env = gym.make("Pendulum-v0")
+        state_size = env.observation_space.shape[0]
+        action_size = 1
+        done = False
+        batch_size = 32
 
+        state = env.reset()
+        state = np.reshape(state, [1, state_size])
+        for t in range(500):
+            env.render()
+            action = agent.act(state)
+            next_state, _, done, _ = env.step([action])
+            next_state = np.reshape(next_state, [1, state_size])
+            state = next_state
+            if done:
+                break
+        time.sleep(1.0)
+        print("Hello from render sleep")
